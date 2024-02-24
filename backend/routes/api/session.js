c529e91e2130317050ecd080d3eaede5c17ac035
a-gehlot <andrew.gehlot@gmail.com>
2024-02-24 16:49:15 -0500

    validating user, pass not blank

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -4,6 +4,20 @@ const router = express.Router();
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation')
+
+const validateLogin = [
+    check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username'),
+    check('password')
+    .exists({ checkFalsy: true})
+    .withMessage('Please provide a password.'),
+    handleValidationErrors
+]
+
 router.get(
     '/',
     restoreUser,
@@ -17,7 +31,7 @@ router.get(
     }
 )
 
-router.post('/', async (req, res, next) => {
+router.post('/', validateLogin, async (req, res, next) => {
     const { credential, password } = req.body;
 
     const user = await User.login({ credential, password });
