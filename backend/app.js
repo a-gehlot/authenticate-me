01b312aa349fb4e8c2e026d4cc79971c77db57f8
a-gehlot <andrew.gehlot@gmail.com>
2024-12-31 14:49:12 -0500

    Error handling middleware

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
+const { ValidationError } = require('sequelize')
+
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 
@@ -37,5 +39,32 @@ app.use(
 const routes = require('./routes');
 
 app.use(routes);
+
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource not found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, _res, _req, next) => {
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = "Validaiton error"
+    }
+    next(err);
+});
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || "Server error",
+        message: err.message,
+        error: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
 
 module.exports = app;
