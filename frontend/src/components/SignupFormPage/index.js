bff291fbe68eeb7385c500556a6f8f13995a9eb8
a-gehlot <andrew.gehlot@gmail.com>
2025-02-09 00:20:16 -0500

    Renamed file

---
+++ frontend/src/components/SignupFormPage/index.js
@@ -0,0 +1,73 @@
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { signupUser } from '../../store/session';
+import { Redirect } from 'react-router-dom'
+
+function SignupFormPage() {
+    const dispatch = useDispatch();
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [errors, setErrors] = useState([]);
+    
+    const user = useSelector((state) => state.session.user);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (password !== confirmPassword) {
+            return setErrors(['Confirm Password field must match'])
+        }
+        await dispatch(signupUser({username, email, password}))
+    }
+
+    if (user) {
+        return <Redirect to="/"/>
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <ul>
+                {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+            </ul>
+            <label>
+                Email
+                <input 
+                    type='text'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+            </label>
+            <label>
+                Username
+                <input 
+                    type='text'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+            </label>
+            <label>
+                Password
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+            </label>
+            <label>
+                Confirm Password
+                <input
+                    type="password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                />
+            </label>
+            <button type="submit">Sign Up</button>
+        </form>
+    )
+}
+
+export default SignupFormPage;
